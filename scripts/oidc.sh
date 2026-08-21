4fd4aa92826157fbaf7301f34fe2e2859e0c9dd6
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2026-08-21 14:17:02 -0300

    chore: add files

--- scripts/oidc.sh
+++ scripts/oidc.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+## Create
 aws iam create-role --role-name github-repo-1341192133 --assume-role-policy-document file://data.json
 aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --role-name github-repo-1341192133
+
+## Rollback
+aws iam detach-role-policy --role-name github-repo-1341192133 --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+aws iam delete-role --role-name github-repo-1341192133
